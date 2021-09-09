996b6ceb1118926f649174427bef9f6fb7a912e1
Jadenir <jadenir.p.s@gmail.com>
2021-09-09 15:23:16 -0300

    Commit final da jornada

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -4,7 +4,9 @@ using AutoMapper;
 using DevReviews.API.Entities;
 using DevReviews.API.Models;
 using DevReviews.API.Persistence.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace DevReviews.API.Controllers
 {
@@ -39,11 +41,28 @@ namespace DevReviews.API.Controllers
             {
                 return NotFound();
             }
+            //Grava um log
+            Log.Information("GET por ID chamado com ID: " + id);
+            
             var productDetails = _mapper.Map<ProductDetailsViewModel>(product);
             return Ok(productDetails);
         }
         //Post para api/products
+        /// <summary>Cadastro de Produto</summary>
+        /// <remarks>Requisição:
+        /// {
+        /// "title": "Um chinelo top"  ,
+        /// "description": "Um chienelo de marca", 
+        /// "price": 100
+        /// }
+        ///</remarks>
+        /// <param name="model">Obejgo com dados de cadastro de produto</param>
+        /// <returns>Objeto recém criado</returns>
+        /// <response code="201">Created</response>
+        /// <response code="400">Bad Request</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Post(AddProductInputModel model)
         {
             var product = new Product(model.Title, model.Desiption, model.Price);
@@ -53,6 +72,12 @@ namespace DevReviews.API.Controllers
             return CreatedAtAction(nameof(GetById), new { id = product.Id }, model);
         }
         //Put para api/products/{id}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, UpdateProductInputModel model)
         {
